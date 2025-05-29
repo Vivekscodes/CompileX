5b94ff00af775dd33e9e3f62291806782cad73f7
Vivekscodes <viveks2026@mpgi.edu.in>
2025-05-29 09:54:37 +0530

    Minor Code Changes

--- server/Actions.js
+++ server/Actions.js
@@ -1,10 +1,22 @@
-const ACTIONS = {
-    JOIN: 'join',
-    JOINED: 'joined',
-    DISCONNECTED: 'disconnected',
-    CODE_CHANGE: 'code-change',
-    SYNC_CODE: 'sync-code',
-    LEAVE: 'leave',
-}
+```javascript
+/**
+ * @description Defines the actions that can occur within the code collaboration environment.
+ */
+const ACTIONS: {
+    JOIN: string;
+    JOINED: string;
+    DISCONNECTED: string;
+    CODE_CHANGE: string;
+    SYNC_CODE: string;
+    LEAVE: string;
+} = {
+    JOIN: 'join', // User joining the room
+    JOINED: 'joined', // User successfully joined the room
+    DISCONNECTED: 'disconnected', // User disconnected from the room
+    CODE_CHANGE: 'code-change', // Code has been changed
+    SYNC_CODE: 'sync-code', // Synchronize code with other users
+    LEAVE: 'leave', // User leaving the room
+};
 
-module.exports = ACTIONS;
+export default ACTIONS;
+```
